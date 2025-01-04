8169f120e5e01235ba6a73567d6244d9ac2da411
Rama Bondan Prakoso <ramanarubp@gmail.com>
2025-01-05 04:46:59 +0700

    X6852: Initial TWRP device tree
    * Patch transsion_touch.ko to load touch module
    
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/infinix/Infinix-X6852
+
+# Inherit from mt6855-common
+include device/transsion/mt6855-common/BoardConfigCommon.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Infinix-X6852
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_Infinix-X6852
+TARGET_RECOVERY_DEVICE_MODULES := libinit_Infinix-X6852
+
+# TWRP Configs
+TW_DEVICE_VERSION := X6852_rama982
